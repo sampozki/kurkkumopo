a5435ffdbc6d88875fb63fa505d4a95a7357b50a
Sampo <pelto.sampo@gmail.com>
2018-09-12 22:30:19 +0300

    Basic structure

--- kurkkumopo.py
+++ kurkkumopo.py
@@ -4,3 +4,33 @@
 # sampozki
 
 # TODO: aivan kaikki
+
+# TODO: Kuvan avaus ja luku
+# TODO: Laitettavan kuvan pienennyt
+# TODO: Kuvan tallennus
+# TODO: PIL kuva in kuv
+# TODO: PIL teksti puhekuplaan
+# TODO: KOPIOI KOODI FUZUBOTILTA
+
+
+def readimage(imagepath):
+    # Lukee kuvan ja palauttaa kuvan
+    pass
+
+
+def addtext(image, text):
+    # Lisää kuvan puhekuplaan tekstiä
+    pass
+
+
+def addimage(image, imagepath):
+    # Lisää subkuvan kuvan sisälle oikeaan kohtaan
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
